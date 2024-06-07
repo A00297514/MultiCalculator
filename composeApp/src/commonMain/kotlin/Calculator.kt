dccac5dea9fac8e52f4b3c9774aa2ac766e9e361
A00297514 <A00297514@mycambrian.ca>
2024-06-08 03:32:06 +0530

    With Calculator file

---
+++ composeApp/src/commonMain/kotlin/Calculator.kt
@@ -0,0 +1,23 @@
+class Calculator {
+
+    // A function which add 2 values
+    fun add(left: Int, right: Int): Int{
+        return left + right
+    }
+
+    //A function which subtract 2 values
+    fun subtract(left: Int, right: Int): Int{
+        return left - right
+    }
+
+    //A function which multiply 2 values
+    fun multiply(left: Int, right: Int): Int{
+        return left * right
+    }
+
+    //A function which divide 2 values
+    fun divide(left: Int, right: Int): Int {
+        return left / right
+    }
+
+}
