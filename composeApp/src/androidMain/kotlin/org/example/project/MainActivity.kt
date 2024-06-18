9f373aac051e936675fd0dab67dba553edc68ae4
A00297514 <A00297514@mycambrian.ca>
2024-06-18 10:44:49 +0530

    All functions declared

--- composeApp/src/androidMain/kotlin/org/example/project/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/org/example/project/MainActivity.kt
@@ -4,8 +4,12 @@ import App
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.ui.tooling.preview.Preview
+import java.lang.reflect.Modifier
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,3 +26,33 @@ class MainActivity : ComponentActivity() {
 fun AppAndroidPreview() {
     App()
 }
+
+@Composable
+fun CalcView(){
+
+}
+
+@Composable
+fun CalcRow(){
+
+}
+
+@Composable
+fun CalcDisplay(){
+
+}
+
+@Composable
+fun CalcNumericButton(){
+
+}
+
+@Composable
+fun CalcOperationButton(){
+
+}
+@Composable
+fun CalcEqualsButton(display: MutableState<String>) {
+
+}
+
